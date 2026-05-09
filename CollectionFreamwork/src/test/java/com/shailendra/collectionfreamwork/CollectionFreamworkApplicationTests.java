373dc20c71d8518f30ec21c564ca14b576d9b4f0
shailendra Barewar <sbarewar91@gmail.com>
2026-05-09 21:38:48 +0530

    newCollection project

---
+++ CollectionFreamwork/src/test/java/com/shailendra/collectionfreamwork/CollectionFreamworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shailendra.collectionfreamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionFreamworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
